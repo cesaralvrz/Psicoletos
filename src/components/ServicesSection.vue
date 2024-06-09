34f389802d282b3205fd2da71cc414dc26a4aab6
César Álvarez <cesaralv97@gmail.com>
2024-06-09 18:34:06 +0200

    Create ServiceSection component

---
+++ src/components/ServicesSection.vue
@@ -0,0 +1,40 @@
+<template>
+	<section class="m-4 md:m-8">
+		<div class="container mx-auto p-4 my-6 space-y-2 text-center">
+			<h2 class="text-5xl font-bold text-psicoletos">Los servicios que ofrecemos</h2>
+			<p class="">Estos son los servicios que ofrecemos en Psicoletos:</p>
+		</div>
+		<div class="container mx-auto grid justify-center gap-4 sm:grid-cols-2 lg:grid-cols-3">
+			<div class="flex flex-col items-center p-4">
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-8 h-8">
+					<path fill-rule="evenodd" d="M11.3 1.046A1 1 0 0112 2v5h4a1 1 0 01.82 1.573l-7 10A1 1 0 018 18v-5H4a1 1 0 01-.82-1.573l7-10a1 1 0 011.12-.38z" clip-rule="evenodd"></path>
+				</svg>
+				<h3 class="my-3 text-3xl font-semibold">Terapia individual para adultos</h3>
+				<div class="space-y-1 leadi text-psicoletos">
+                    <p>Terapia Cognitivo-Conductual.</p>
+					<p>Manejo de la ansiedad y el estrés, depresión, relaciones, desarrollo personal, entre otros.</p>
+				</div>
+			</div>
+			<div class="flex flex-col items-center p-4">
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-8 h-8">
+					<path fill-rule="evenodd" d="M11.3 1.046A1 1 0 0112 2v5h4a1 1 0 01.82 1.573l-7 10A1 1 0 018 18v-5H4a1 1 0 01-.82-1.573l7-10a1 1 0 011.12-.38z" clip-rule="evenodd"></path>
+				</svg>
+				<h3 class="my-3 text-3xl font-semibold">Psicoterapia para niños y adolescentes</h3>
+				<div class="space-y-1 leadi text-psicoletos">
+					<p>Intervención psicológica a niños y adolescentes. Asesoramiento a padres y familiares.</p>
+                    <p>Ansiedad infantil, problemas de comportamiento, autoestima y confianza.</p>
+				</div>
+			</div>
+			<div class="flex flex-col items-center p-4">
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-8 h-8 ">
+					<path fill-rule="evenodd" d="M11.3 1.046A1 1 0 0112 2v5h4a1 1 0 01.82 1.573l-7 10A1 1 0 018 18v-5H4a1 1 0 01-.82-1.573l7-10a1 1 0 011.12-.38z" clip-rule="evenodd"></path>
+				</svg>
+				<h3 class="my-3 text-3xl font-semibold">Guías y webinars psicológicas</h3>
+				<div class="space-y-1 leadi text-psicoletos">
+					<p>Contenido audivisual sobre diversos temas psicológicos y desarrollo personal.</p>
+                    <p>Materiales prácticos y accesibles para mejorar la salud mental.</p>
+				</div>
+			</div>
+		</div>
+	</section>
+</template>
