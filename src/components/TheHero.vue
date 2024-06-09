fcb31064aba98b18e85efb36fc484ed5855868c5
César Álvarez <cesaralv97@gmail.com>
2024-06-09 18:33:43 +0200

    Create TheHero component

---
+++ src/components/TheHero.vue
@@ -0,0 +1,13 @@
+<template>
+	<section class="">
+		<div class="container mx-auto flex flex-col items-center px-4 py-16 text-center md:py-32 md:px-10 lg:px-32 xl:max-w-3xl">
+			<h1 class="text-6xl font-bold sm:text-7xl">Conecta con tu 
+				<span class="text-psicoletos">bienestar</span>
+			</h1>
+			<p class="px-8 mt-8 mb-12 text-xl text-psicoletos">Un espacio seguro y personalizado para explorar y mejorar tu bienestar personal</p>
+			<div class="flex flex-wrap justify-center">
+				<button class="px-8 py-3 m-2 text-lg border rounded dark:text-gray-50 dark:border-gray-700">Learn more</button>
+			</div>
+		</div>
+	</section>
+</template>
