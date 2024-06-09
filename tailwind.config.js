985d32ce2681c787118273b913c74c894a0b94e2
César Álvarez <cesaralv97@gmail.com>
2024-06-09 18:33:05 +0200

    Add psicoletos color to config tailwind file

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        psicoletos: "#565766",
+      },
+    }
   },
   plugins: [],
 }
