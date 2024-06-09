583e1eced19b566be52e3c75a923f758727d510b
César Álvarez <cesaralv97@gmail.com>
2024-06-09 20:00:37 +0200

    Create TheFooter component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <template>
   <the-nav />
-  <the-hero />
-  <services-section />
-  <about-me-section />
+  <div class="max-w-screen-xl mx-auto p-8 text-center">
+    <the-hero />
+    <services-section />
+    <about-me-section />
+    <contact-section />
+  </div>
+  <the-footer />
 </template>
 
 <script setup>
@@ -10,4 +14,6 @@ import TheNav from "./components/TheNav.vue"
 import TheHero from "./components/TheHero.vue"
 import ServicesSection from "./components/ServicesSection.vue"
 import AboutMeSection from "./components/AboutMeSection.vue"
+import ContactSection from "./components/ContactSection.vue"
+import TheFooter from "./components/TheFooter.vue"
 </script>
---
+++ src/components/TheFooter.vue
@@ -0,0 +1,28 @@
+<template>
+	<footer class="px-4 py-8 bg-pinky text-gray-800">
+		<div class="container flex flex-wrap items-center justify-center mx-auto space-y-4 sm:justify-between sm:space-y-0">
+			<div class="flex flex-row pr-3 space-x-4 sm:space-x-8">
+				<img src="../assets/logo_psicoletos.png" alt="Logo Psicoletos" class="object-scale-down max-h-full">
+				<ul class="flex flex-wrap items-center space-x-4 sm:space-x-8">
+					<li>
+						<a rel="noopener noreferrer" href="#">Terms of Use</a>
+					</li>
+					<li>
+						<a rel="noopener noreferrer" href="#">Privacy</a>
+					</li>
+				</ul>
+			</div>
+			<ul class="flex flex-wrap pl-3 space-x-4 sm:space-x-8">
+				<li>
+					<a rel="noopener noreferrer" href="#">Instagram</a>
+				</li>
+				<li>
+					<a rel="noopener noreferrer" href="#">Facebook</a>
+				</li>
+				<li>
+					<a rel="noopener noreferrer" href="#">Twitter</a>
+				</li>
+			</ul>
+		</div>
+	</footer>
+</template>
