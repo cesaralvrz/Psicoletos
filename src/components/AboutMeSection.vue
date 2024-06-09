01ceff0d3f0f2e6f158a0f7e0a3f1e52b220edaa
César Álvarez <cesaralv97@gmail.com>
2024-06-09 18:34:45 +0200

    Create AboutMeSection component

---
+++ src/components/AboutMeSection.vue
@@ -0,0 +1,38 @@
+<template>
+<section class="mt-32">
+    <div class="relative flex">
+        <div class="min-h-screen lg:w-1/3"></div>
+        <div class="hidden w-3/4 min-h-screen rounded bg-gray-100 lg:block"></div>
+        <div
+            class="container flex flex-col justify-center w-full min-h-screen px-6 py-10 mx-auto lg:absolute lg:inset-x-0">
+            <h1 class="text-4xl font-semibold capitalize lg:text-3xl">
+                Sobre <span class="text-psicoletos">Mí:</span>
+            </h1>
+
+            <div class="mt-10 lg:mt-20 lg:flex">
+                <img class="object-cover object-center w-full lg:w-[32rem] rounded-lg h-96" src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" alt="">
+
+                <div class="mt-8 lg:px-10 lg:mt-0">
+                    <h1 class="text-2xl font-semibold mb-8">
+                        ¡Hola! Mi nombre es Carla Álvarez Llaneza.
+                    </h1>
+
+                    <p class="max-w-lg mt-6 text-psicoletos text-left">
+                        Soy psicóloga sanitaria (Nº M-37087) con amplia experiencia en el campo de la salud mental. A lo largo de mi carrera, he tenido el privilegio de trabajar con una amplia variedad de poblaciones, incluyendo adultos, adolescentes, niños y familias, ayudándolos a enfrentar y superar diversos desafíos emocionales y psicológicos.
+                    </p>
+
+                    <p class="max-w-lg mt-6 text-psicoletos text-left">
+                        Mi enfonque terapéutico se basa en la Terapia Cognitivo-Conductual (TCC). Utilizo una combinación de técnicas basadas en la evidencia para ayudar a mis pacientis para desarrollar prácticas para manejar el estrés, la ansiedad, la depresión y otros problemas de salud mental.
+                    </p>
+
+                    <p class="max-w-lg mt-6 text-psicoletos text-left">
+                        Además de mi práctica clínica, me dedico a la educación y divulgación de la psicología a través de redes sociales, webinars, talleres, etc. 
+                    </p>
+
+                    <h3 class="mt-8 text-lg font-medium w-[32rem]">Mi objetivo es desmitificar la salud mental y proporcionar herramientas accesibles para que todos puedan cuidar de su bienestar psicológico.</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+</template>
