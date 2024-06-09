23ab2ded35d1b651b2cc802b0dbdf95baebc2745
César Álvarez <cesaralv97@gmail.com>
2024-06-09 18:34:23 +0200

    Create TheNav component

---
+++ src/components/TheNav.vue
@@ -0,0 +1,23 @@
+<template>
+	<header class="p-4">
+		<div class="container flex justify-between h-16 mx-auto text-psicoletos">
+			<img src="../assets/logo_psicoletos.png" alt="Logo Psicoletos" class="object-scale-down max-h-full">
+			<ul class="items-stretch hidden space-x-3 md:flex text-xl">
+				<li class="flex">
+					<a rel="noopener noreferrer" href="#" class="flex items-center px-4 -mb-1 border-b-2 dark:border-transparent">Info</a>
+				</li>
+				<li class="flex">
+					<a rel="noopener noreferrer" href="#" class="flex items-center px-4 -mb-1 border-b-2 dark:border-transparent">Servicios</a>
+				</li>
+				<li class="flex">
+					<a rel="noopener noreferrer" href="#" class="flex items-center px-4 -mb-1 border-b-2 dark:border-transparent">Contacto</a>
+				</li>
+			</ul>
+			<button class="flex justify-end p-4 md:hidden">
+				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-6 h-6">
+					<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
+				</svg>
+			</button>
+		</div>
+	</header>
+</template>
