9031361addfc5fc3b96ff51cdd090e1fe7e952dd
César Álvarez <cesaralv97@gmail.com>
2024-06-09 20:00:18 +0200

    Create ContactSection component

--- src/components/ContactSection.vue
+++ src/components/ContactSection.vue
@@ -1,9 +1,9 @@
 <template>
 	<section class="py-44">
 		<div class="grid max-w-6xl grid-cols-1 px-6 mx-auto lg:px-8 md:grid-cols-2 md:divide-x divide-black">
-			<div class="py-6 md:py-0 md:px-6">
-				<h1 class="text-4xl font-bold">Get in touch</h1>
-				<p class="pt-2 pb-4 text-psicoletos">Fill in the form to start a conversation</p>
+			<div class="py-6 md:py-0 md:px-6 my-auto">
+				<h1 class="text-4xl font-bold">Ponte en contacto con nosotros</h1>
+				<p class="pt-2 pb-4 text-lg text-psicoletos">Si estás buscando apoyo psicológico te animo a que te pongas en contacto con nosotros para explorar cómo podemos trabajar juntos hacia tu bienestar mental y emocional.</p>
 			</div>
 			<form class="flex flex-col py-6 space-y-6 md:py-0 md:px-6 text-left">
 				<label class="block">
@@ -11,11 +11,19 @@
 					<input type="text" placeholder="Introduce tu nombre..." class="block w-full rounded-md shadow-sm focus:ring focus:ri focus:ri">
 				</label>
 				<label class="block">
-					<span class="mb-1">Emails:</span>
+					<span class="mb-1">Email:</span>
 					<input type="email" placeholder="Introduce tu email..." class="block w-full rounded-md shadow-sm focus:ring focus:ri focus:ri">
 				</label>
 				<label class="block">
-					<span class="mb-1">Mensaje:</span>
+					<span class="mb-1">Teléfono:</span>
+					<input type="text" placeholder="Introduce tu número de teléfono..." class="block w-full rounded-md shadow-sm focus:ring focus:ri focus:ri">
+				</label>
+				<label class="block">
+					<span class="mb-1">¿Qué servicio necesitas?</span>
+					<input type="text" placeholder="Introduce tu número de teléfono..." class="block w-full rounded-md shadow-sm focus:ring focus:ri focus:ri">
+				</label>
+				<label class="block">
+					<span class="mb-1">Cuéntanos más a detalle:</span>
 					<textarea rows="3" class="block w-full rounded-md focus:ring focus:ri focus:ri"></textarea>
 				</label>
 				<button type="button" class="self-center px-8 py-3 text-lg text-white rounded focus:ring hover:ring focus:ri focus:ri hover:ri">Enviar</button>
